faae6c9eff5df58a0edbe5f04bf99ec1744e6c69
coinleft <submit@aliyun.com>
2025-11-15 00:11:45 +0800

    init

---
+++ test.py
@@ -0,0 +1,8 @@
+import langchain_community
+print(langchain_community.__file__)
+
+import langchain_text_splitters 
+
+print(langchain_text_splitters.__file__)
+# /opt/anaconda3/envs/langchain/lib/python3.13/site-packages/langchain_community/__init__.py
+# /opt/anaconda3/envs/langchain/lib/python3.13/site-packages/langchain_text_splitters/__init__.py
